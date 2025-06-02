cce02a283d4e235bfa02ca7bcbbc8b3e06653bd7
jimmyc24942 <jimmyc24942@masseyhigh.school.nz>
2025-06-03 09:25:33 +1200

    Working statement generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# Functions go here
+def statement_generator (statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Main route goes here
+statement_generator("Instructions", "-")
